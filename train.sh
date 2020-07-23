8acdd4475156df7252b2e78c97c3740d8ab00634
ofri masad <ofri@camera51.com>
2020-07-23 21:57:53 +0300

    added dataset and settings

---
+++ train.sh
@@ -0,0 +1,9 @@
+python train.py --dataset-dir "./data/div2k" \
+                --output-dir "./results/output" \
+                --model "WDSR-A" \
+                --scale 2 \
+                --n-feats 32 \
+                --n-res-blocks 16 \
+                --expansion-ratio 4 \
+                --res-scale 1.0 \
+                --lr 1e-3
