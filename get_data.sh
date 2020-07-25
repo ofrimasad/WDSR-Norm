8bf9badcc1ee7872ced2ce029e7dadfbd00369b9
ofri masad <ofri@camera51.com>
2020-07-25 11:03:08 +0300

    fix download script

--- get_data.sh
+++ get_data.sh
@@ -1,19 +1,17 @@
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X2.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X3.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X4.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X2.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X3.zip .
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X4.zip .
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip -P ./data/div2k/
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X2.zip -P ./data/div2k/
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X3.zip -P ./data/div2k/
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_LR_bicubic_X4.zip -P ./data/div2k/
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip -P ./data/div2k/
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X2.zip -P ./data/div2k/
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X3.zip -P ./data/div2k/
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X4.zip -P ./data/div2k/
 
-unzip DIV2K_train_HR.zip
-unzip DIV2K_train_HR.zip
-unzip DIV2K_train_LR_bicubic_X2.zip
-unzip DIV2K_train_LR_bicubic_X3.zip
-unzip DIV2K_train_LR_bicubic_X4.zip
-unzip DIV2K_valid_HR.zip
-unzip DIV2K_valid_LR_bicubic_X2.zip
-unzip DIV2K_valid_LR_bicubic_X3.zip
-unzip DIV2K_valid_LR_bicubic_X4.zip
+unzip ./data/div2k/DIV2K_train_HR.zip -d ./data/div2k/
+unzip ./data/div2k/DIV2K_train_LR_bicubic_X2.zip -d ./data/div2k/
+unzip ./data/div2k/DIV2K_train_LR_bicubic_X3.zip -d ./data/div2k/
+unzip ./data/div2k/DIV2K_train_LR_bicubic_X4.zip -d ./data/div2k/
+unzip ./data/div2k/DIV2K_valid_HR.zip -d ./data/div2k/
+unzip ./data/div2k/DIV2K_valid_LR_bicubic_X2.zip -d ./data/div2k/
+unzip ./data/div2k/DIV2K_valid_LR_bicubic_X3.zip -d ./data/div2k/
+unzip ./data/div2k/DIV2K_valid_LR_bicubic_X4.zip -d ./data/div2k/
