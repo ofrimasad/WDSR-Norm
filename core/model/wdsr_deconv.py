fa4325684878a5e04b17694d8f0ae401aad8a7ae
ofri masad <ofri@camera51.com>
2020-07-30 08:51:32 +0300

    bug fix in deconv layers for scale > 2

--- core/model/wdsr_deconv.py
+++ core/model/wdsr_deconv.py
@@ -21,8 +21,8 @@ class WDSR_Deconv(nn.Module):
         super(WDSR_Deconv, self).__init__()
         head = [weight_norm(nn.Conv2d(3, args.n_feats, kernel_size=3, padding=1))]
         body = [ResBlock(args.n_feats, args.expansion_ratio, args.res_scale) for _ in range(args.n_res_blocks)]
-        tail = [nn.ConvTranspose2d(args.n_feats, 3, kernel_size=3, stride=args.scale, padding=1, output_padding=1)]
-        skip = [nn.ConvTranspose2d(3, 3, kernel_size=5, stride=args.scale, padding=2, output_padding=1)]
+        tail = [nn.ConvTranspose2d(args.n_feats, 3, kernel_size=3, stride=args.scale, padding=1, output_padding=args.scale-1)]
+        skip = [nn.ConvTranspose2d(3, 3, kernel_size=5, stride=args.scale, padding=2, output_padding=args.scale-1)]
 
         self.head = nn.Sequential(*head)
         self.body = nn.Sequential(*body)
