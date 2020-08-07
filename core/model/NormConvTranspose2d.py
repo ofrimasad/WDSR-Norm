fcf71f588e0a627347026e1426e7540fd23946e0
ofri masad <ofri@camera51.com>
2020-08-07 17:31:59 +0300

    change norm implementation

--- core/model/NormConvTranspose2d.py
+++ core/model/NormConvTranspose2d.py
@@ -1,18 +1,15 @@
 from torch import nn
 import torch
 
-class NormConvTranspose2d(nn.Module):
 
-    def __init__(self,  *args, **kwargs):
-        super(NormConvTranspose2d, self).__init__()
-        self.transpose2d = nn.ConvTranspose2d(*args, **kwargs)
+class NormConvTranspose2d(nn.ConvTranspose2d):
 
-    def forward(self, x):
-        ones = torch.ones_like(x)
+    def forward(self, input, output_size=None):
+        ones = torch.ones_like(input)
         ones.requires_grad = False
-        x = self.transpose2d(x)
-        normalizer = self.transpose2d(ones)
-        x = x/normalizer
+        x = super().forward(input, output_size)
+        normalizer = super().forward(ones, output_size)
+        x = x / normalizer
 
         return x
 
