026d9b82b16567fbffd17f40c6104c42f6b9f2ef
ofrimasad <ofri@deci.ai>
2020-09-20 10:05:30 +0300

    new implementation

--- core/model/NormConvTranspose2d.py
+++ core/model/NormConvTranspose2d.py
@@ -7,13 +7,13 @@ eps = 1e-10
 T = int
 
 
-class NormConvTranspose2d(nn.Module):
+class NormConvTranspose2d_old(nn.Module):
 
     def __init__(self, in_channels: int, out_channels: int, kernel_size: Union[T, Tuple[T, T]],
                  stride: Union[T, Tuple[T, T]] = 1, padding: Union[T, Tuple[T, T]] = 0,
                  output_padding: Union[T, Tuple[T, T]] = 0, groups: int = 1, bias: bool = True, dilation: int = 1,
                  padding_mode: str = 'zeros'):
-        super(NormConvTranspose2d, self).__init__()
+        super(NormConvTranspose2d_old, self).__init__()
 
         if groups != 1:
             raise NotImplementedError('NormConvTranspose2d is currently not implemented from groups != 1')
@@ -51,5 +51,54 @@ class NormConvTranspose2d(nn.Module):
         for i in range(self.out_channels):
             self.sub_convs[i].weight.data = tensor[i].unsqueeze(1)
 
+
+class NormConvTranspose2d(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: Union[T, Tuple[T, T]],
+                 stride: Union[T, Tuple[T, T]] = 1, padding: Union[T, Tuple[T, T]] = 0,
+                 output_padding: Union[T, Tuple[T, T]] = 0, groups: int = 1, bias: bool = True, dilation: int = 1,
+                 padding_mode: str = 'zeros'):
+        super(NormConvTranspose2d, self).__init__()
+
+        if groups != 1:
+            raise NotImplementedError('NormConvTranspose2d is currently not implemented from groups != 1')
+
+        self.out_channels = out_channels
+
+        if bias:
+            self._bias = nn.Parameter(torch.zeros(out_channels))
+
+        self.deconv = nn.ConvTranspose2d(in_channels, in_channels * out_channels, kernel_size, stride, padding,
+                                                     output_padding=output_padding, groups=in_channels, bias=False,
+                                                     dilation=dilation, padding_mode=padding_mode)
+        self.sum = nn.Conv2d(in_channels * out_channels, out_channels, 1, 1, 0, 1, out_channels, False)
+        self.sum.weight.data.fill_(1)
+        self.sum.weight.requires_grad = False  # we train the sum bias only
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.ones = None
+        # self.add_module("deconv", self.deconv)
+
+
+    def forward(self, input, output_size=None):
+
+        self.sum.weight.requires_grad = False # we train the sum bias only
+
+        if self.ones is None or self.ones.shape != input.shape:
+            self.ones = torch.ones_like(input).to(input.device)
+
+        x = self.deconv(input)
+        normalizer = self.deconv(self.ones)
+        x /= normalizer
+        x = x.reshape(x.shape[0], self.in_channels, self.out_channels, x.shape[2], x.shape[3])
+        x = torch.transpose(x, 1, 2)
+        x = x.reshape(x.shape[0], self.in_channels * self.out_channels, x.shape[3], x.shape[4])
+        x = self.sum(x)
+
+        return x
+
+    def fill(self, tensor):
+
+        self.deconv.weight.data = tensor
 
 
